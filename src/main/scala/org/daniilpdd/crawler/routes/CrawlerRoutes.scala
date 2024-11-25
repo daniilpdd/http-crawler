d9d737ba393bea6ad7567a4e54557507ffd3be56
daniilpdd <Daniil.Poddubnyi@aton.ru>
2024-11-25 17:00:57 +0300

    add min logs

--- src/main/scala/org/daniilpdd/crawler/routes/CrawlerRoutes.scala
+++ src/main/scala/org/daniilpdd/crawler/routes/CrawlerRoutes.scala
@@ -2,6 +2,7 @@ package org.daniilpdd.crawler.routes
 
 import org.daniilpdd.crawler.service.CrawlerService
 import org.daniilpdd.crawler.service.client.RequestService
+import zio.ZIO
 import zio.http.{Method, Request, Response, Routes, Status, handler}
 
 object CrawlerRoutes {
@@ -9,11 +10,12 @@ object CrawlerRoutes {
     Method.POST / "api" / "urls" -> handler { req: Request =>
       (for {
         urls <- req.body.asString.map(_.split('\n').map(_.trim).filter(_.nonEmpty).distinct)
+        _ <- ZIO.logDebug(urls.mkString("(", ",", ")"))
+        _ <- ZIO.logInfo(s"urls size: ${urls.length}")
         res <- CrawlerService.getTitles(urls)
       } yield Response.text(res.mkString("\n"))).orElseFail({
         Response.error(Status.InternalServerError)
       })
     }
   )
-
 }
