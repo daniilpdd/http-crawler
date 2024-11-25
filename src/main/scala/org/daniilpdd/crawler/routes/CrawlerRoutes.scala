b1a02d86741d60e7352b563721a59091ca7c1a3a
daniilpdd <Daniil.Poddubnyi@aton.ru>
2024-11-25 17:23:40 +0300

    add get endpoint, CrawlerService and RequestService work with zio.http.URL

--- src/main/scala/org/daniilpdd/crawler/routes/CrawlerRoutes.scala
+++ src/main/scala/org/daniilpdd/crawler/routes/CrawlerRoutes.scala
@@ -2,18 +2,30 @@ package org.daniilpdd.crawler.routes
 
 import org.daniilpdd.crawler.service.CrawlerService
 import org.daniilpdd.crawler.service.client.RequestService
-import zio.http.{Method, Request, Response, Routes, Status, handler}
+import zio.ZIO
+import zio.http.codec.TextCodec.StringCodec
+import zio.http.{Method, Request, Response, Routes, Status, URL, handler}
 
 object CrawlerRoutes {
   def routes(): Routes[RequestService, Nothing] = Routes(
     Method.POST / "api" / "urls" -> handler { req: Request =>
       (for {
-        urls <- req.body.asString.map(_.split('\n').map(_.trim).filter(_.nonEmpty).distinct)
+        urlsStr <- req.body.asString.map(_.split('\n').map(_.trim).filter(_.nonEmpty).distinct)
+        urls <- ZIO.foreach(urlsStr)(u => ZIO.fromEither(URL.decode(u)))
+        res <- CrawlerService.getTitles(urls)
+      } yield Response.text(res.mkString("\n"))).orElseFail({
+        Response.error(Status.InternalServerError)
+      })
+    },
+    Method.GET / "api" / "urls" -> handler { req: Request =>
+      (for {
+        queryParam <- req.queryParamToZIO("urls")(StringCodec)
+        encodedUrls <- ZIO.attempt(queryParam.split(',').map(_.trim).filter(_.nonEmpty))
+        urls <- ZIO.foreach(encodedUrls)(u => ZIO.fromEither(URL.decode(u)))
         res <- CrawlerService.getTitles(urls)
       } yield Response.text(res.mkString("\n"))).orElseFail({
         Response.error(Status.InternalServerError)
       })
     }
   )
-
 }
