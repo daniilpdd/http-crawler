a4fde0e4317c6f1ee173d6dc90e86d76ccc993d6
daniilpdd <daniil.poddubnyi@gmail.com>
2024-11-25 23:34:20 +0500

    general error handling in routes

--- src/main/scala/org/daniilpdd/crawler/routes/CrawlerRoutes.scala
+++ src/main/scala/org/daniilpdd/crawler/routes/CrawlerRoutes.scala
@@ -3,26 +3,24 @@ package org.daniilpdd.crawler.routes
 import org.daniilpdd.crawler.routes.wrapper.ResponseWrapper
 import org.daniilpdd.crawler.service.CrawlerService
 import org.daniilpdd.crawler.service.client.RequestService
-import zio.ZIO
+import zio.{UIO, ZIO}
 import zio.http.codec.TextCodec.StringCodec
 import zio.http.{Method, Request, Response, Routes, Status, URL, handler}
 
 object CrawlerRoutes {
-  def routes(): Routes[RequestService with ResponseWrapper, Nothing] = Routes(
+  val routes: Routes[ResponseWrapper with RequestService, Throwable] = Routes(
     Method.POST / "api" / "urls" -> handler { req: Request =>
-      (for {
+      for {
         urlsStr <- req.body.asString.map(_.split('\n').map(_.trim).filter(_.nonEmpty).distinct)
         urls <- ZIO.foreach(urlsStr)(u => ZIO.fromEither(URL.decode(u)))
         _ <- ZIO.logDebug(urls.mkString("(", ",", ")"))
         _ <- ZIO.logInfo(s"urls size: ${urls.length}")
         res <- CrawlerService.getTitles(urls)
         response <- ResponseWrapper.wrap(res)
-      } yield response).orElseFail({
-        Response.error(Status.InternalServerError)
-      })
+      } yield response
     },
     Method.GET / "api" / "urls" -> handler { req: Request =>
-      (for {
+      for {
         queryParam <- req.queryParamToZIO("urls")(StringCodec)
         encodedUrls <- ZIO.attempt(queryParam.split(',').map(_.trim).filter(_.nonEmpty))
         urls <- ZIO.foreach(encodedUrls)(u => ZIO.fromEither(URL.decode(u)))
@@ -30,9 +28,12 @@ object CrawlerRoutes {
         _ <- ZIO.logInfo(s"urls size: ${urls.length}")
         res <- CrawlerService.getTitles(urls)
         response <- ResponseWrapper.wrap(res)
-      } yield response).orElseFail({
-        Response.error(Status.InternalServerError)
-      })
+      } yield response
     }
   )
+
+  val handleError: Throwable => UIO[Response] = throwable => for {
+    _ <- ZIO.logError(throwable.getMessage)
+    response <- ZIO.succeed(Response.error(Status.InternalServerError))
+  } yield response
 }
